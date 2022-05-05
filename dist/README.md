5f97abc5d72f41851f3c4b17ace82b3487d7f4db
Mcnoor <99060667+Mcnoor@users.noreply.github.com>
2022-05-04 21:13:08 -0500

    adding

---
+++ dist/README.md
@@ -0,0 +1,49 @@
+<h1 align="center">ReadMe Generator using node.js 👋 </h1>
+  
+![badge](https://img.shields.io/badge/license-MIT-brightgreen)
+
+## Description
+
+ℹ️ A node.hs application that creates a generated ReadME.md file to the dist directory
+
+## Table of Contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+💾 npm install inquirer is required
+
+## Usage
+
+💻 📱 Project Challenge for Class
+
+## License
+
+![badge](https://img.shields.io/badge/license-MIT-brightgreen)
+
+This application is covered by the MIT license.
+
+## Contributing
+
+👥 👥 MCX
+
+## Tests
+
+📝 No test requested
+
+## Questions
+
+❓❓❓ eamil or reach out
+
+:octocat: Find me on GitHub: [at#yaho.com](https://github.com/at#yaho.com)>
+
+📧 Email me with any questions: gos@tao.xom
+
+_This README was generated with ❤️ by [README-generator] 🔥🔥🔥_
---
+++ utils/generateReadme.js
@@ -0,0 +1,48 @@
+// generateReadme function populating the README.md
+function generateReadme(answers) {
+    return `
+<h1 align="center">${answers.projectTitle} 👋</h1>
+  
+![badge](https://img.shields.io/badge/license-${answers.license}-brightgreen)<br />
+
+## Description
+ℹ️ ${answers.description}
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+💾 ${answers.installation}
+
+## Usage
+💻 📱 ${answers.usage}
+
+## License
+![badge](https://img.shields.io/badge/license-${answers.license}-brightgreen)
+<br />
+This application is covered by the ${answers.license} license. 
+
+## Contributing
+👥 👥 ${answers.contributing}
+
+## Tests
+✏️ ${answers.tests}
+
+## Questions
+❓❓❓ ${answers.questions}<br />
+<br />
+:octocat: Find me on GitHub: [${answers.username}](https://github.com/${answers.username})<br />
+<br />
+✉️ Email me with any questions: ${answers.email}<br /><br />
+
+_This README was generated with ❤️ by [README-generator] 🔥🔥🔥_
+    `;
+  }
+  
+  module.exports = generateReadme;
